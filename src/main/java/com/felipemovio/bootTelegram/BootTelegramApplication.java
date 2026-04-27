29f7ba021568306a0a1db32449514f7c0adfc605
FelipeMovio <felipelsmovio@gmail.com>
2026-04-27 13:33:47 -0300

    inical dependencias

---
+++ src/main/java/com/felipemovio/bootTelegram/BootTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.felipemovio.bootTelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTelegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipemovio/bootTelegram/BootTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemovio.bootTelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
