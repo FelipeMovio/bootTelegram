f92e209af953110d80d1568525ae442a0329bf9b
FelipeMovio <felipelsmovio@gmail.com>
2026-04-28 15:55:26 -0300

    dados bot

---
+++ src/main/java/com/felipemovio/bootTelegram/model/DadosBot.java
@@ -0,0 +1,8 @@
+package com.felipemovio.bootTelegram.model;
+
+public class DadosBot {
+
+    public static final String BOT_TOKEN = "8692997347:AAHHaYd62eS8RVJOd8mHBjTBS9YR_fNXDtc";
+
+    public static final String BOT_USER_NAME = "fMovio_bot";
+}
